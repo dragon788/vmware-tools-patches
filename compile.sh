82fe78429c1ca01ab7b7ee1dd500b41d37852a7d
dragon788 <dragon788@users.noreply.github.com>
2016-01-27 16:34:06 -0600

    Making ANSWERS optional and redirecting to install
    This should supply the correct options after the vmware-install.pl parses the force options.

--- compile.sh
+++ compile.sh
@@ -13,9 +13,17 @@ if hash vmware-uninstall-tools.pl >/dev/null 2>&1; then
   sudo vmware-uninstall-tools.pl
 fi
 
+pushd vmware-tools-distrib >/dev/null
 
-VMWARE_INSTALL_OPTIONS="--clobber-kkernel-modules=pvscsi,vmblock,vmci,vmhgfs,vmmemctl,vmsync,vmxnet,vmxnet3,vsock"
-ANSWERS="--default"
+if hash systemctl >/dev/null 2>&1; then
+  echo "Creating empty init dirs for backwards compatibility"
+  for x in {0..6}; do mkdir -p /etc/init.d/rc${x}.d; done
+  sudo cp $SCRIPT_DIR/patches/vmware-tools.service /etc/systemd/system/
+  sudo systemctl enable vmware-tools.service
+  echo "Added and enabled VMware Tools systemd service"
+fi
+
+VMWARE_INSTALL_OPTIONS="--clobber-kernel-modules=pvscsi,vmblock,vmci,vmhgfs,vmmemctl,vmsync,vmxnet,vmxnet3,vsock"
 
 # The number of answers required changes between tools versions
 # future work is to come up with simple defaults for each tools version
@@ -37,20 +45,14 @@ if [[ -n "$1" ]]; then
   fi
 fi
 
-pushd vmware-tools-distrib >/dev/null
-
-if hash systemctl >/dev/null 2>&1; then
-  echo "Creating empty init dirs for backwards compatibility"
-  for x in {0..6}; do mkdir -p /etc/init.d/rc${x}.d; done
-  sudo cp $SCRIPT_DIR/patches/vmware-tools.service /etc/systemd/system/
-  sudo systemctl enable vmware-tools.service
-  echo "Added and enabled VMware Tools systemd service"
-fi
-
 if sudo ./vmware-install.pl --help 2>&1 | grep -q 'force-install'; then
     VMWARE_INSTALL_OPTIONS="--force-install ${VMWARE_INSTALL_OPTIONS}"
 fi
 
-sudo ./vmware-install.pl ${ANSWERS} ${VMWARE_INSTALL_OPTIONS}
+if [[ -n "${ANSWERS}" ]]; then
+  sudo ./vmware-install.pl ${VMWARE_INSTALL_OPTIONS} < ${ANSWERS} 
+else
+  sudo ./vmware-install.pl --default ${VMWARE_INSTALL_OPTIONS} 
+fi
 
 popd >/dev/null
